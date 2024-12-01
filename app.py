6be100fef1d6d5fc116ced5016958322799a7a4e
BTemplar <97474689+BTemplar@users.noreply.github.com>
2024-12-01 23:13:56 +0400

    Added registration mechanism and user authorization.
    Added database model for users. Added register.html and login.html page templates. Added forms for user registration and authorization.
    Updated requirements.txt.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, redirect, url_for, request, flash
+from flask_login import login_required, logout_user
 import requests
 import socket
 import time
 import smtplib
 from email.mime.text import MIMEText
-from database import init_db, ServiceStatus, db
+from database import init_db, ServiceStatus, db, User
+from forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, LoginManager
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///services.db'
 init_db(app)
 
@@ -15,6 +19,15 @@ SERVICES = [
     ['example', 'https://example.com'],
     ['127.0.0.1','127.0.0.1:37']
 ] # Your services name and services here
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 def check_port(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -92,7 +105,47 @@ def monitor_services():
                 else:
                     send_email(f"Service {service_url[0]} is online", f"The service {service_url[0]} is now online at {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You have successfully registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/', methods=['GET'])
+@login_required
 def index():
     services = ServiceStatus.query.order_by(ServiceStatus.timestamp.desc()).group_by(ServiceStatus.service_url).all()
     services_sla = ServiceStatus.query.all()
@@ -109,6 +162,7 @@ def index():
     return render_template('index.html', services=services, total_responses=total_responses, online_count=online_count, sla=f"{sla:.2f}%", average_response_time=f"{average_response_time:.2f} ms" if average_response_time is not None else None)
 
 @app.route('/check_services', methods=['GET'])
+@login_required
 def check_services_route():
     with app.app_context():
         return jsonify(check_services())
@@ -118,6 +172,7 @@ scheduler.add_job(func=monitor_services, trigger='interval', seconds=300) # Set 
 scheduler.start()
 
 @app.route('/sla', methods=['GET'])
+@login_required
 def sla():
     services = ServiceStatus.query.all()
     total_responses = len(services)
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -9,10 +11,24 @@ class ServiceStatus(db.Model):
     status = db.Column(db.String(10), nullable=False)
     response_time = db.Column(db.Float)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    last_status_change = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
     __table_args__ = (
-        db.UniqueConstraint('service_name', 'service_url', 'timestamp', name='uix_service_url_timestamp'),
+        db.UniqueConstraint('service_url', 'timestamp', name='uix_service_url_timestamp'),
     )
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 def init_db(app):
     db.init_app(app)
