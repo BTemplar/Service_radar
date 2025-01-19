e1d508a588fb8c97bb94137933eb74b187ed91cf
BTemplar <97474689+BTemplar@users.noreply.github.com>
2025-01-20 00:20:21 +0400

    Add location function
    Added geolocation.py file with a function to get additional information about the target service.

---
+++ location.py
@@ -0,0 +1,20 @@
+import urllib.request as urllib2
+import json
+
+def get_location(ip):
+    url = "http://ip-api.com/json/"
+    response = urllib2.urlopen(url + ip)
+    data = response.read()
+    values = json.loads(data)
+
+    return values
+
+ip = input("What is your target IP: ")
+values = get_location(ip)
+
+print("IP: " + values["query"])
+print("City: " + values["city"])
+print("ISP: " + values["isp"])
+print("Country: " + values["country"])
+print("Region: " + values["region"])
+print("Timezone: " + values["timezone"])
